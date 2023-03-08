e16b8538965aa43c07621135ad42aec323dedff2
JesusAParedes <paredesjesus93@gmail.com>
2023-03-07 19:25:20 -0600

    a filter function that stores the liked beers is created and a button that changes to liked is made. not the best

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import BeerCard from './BeerCard';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       beerArray : [],
+      likedBeers: [],
+      isLiked: false,
+      text: 'Like this beer?'
     };
   }
-
- 
 
   componentDidMount() {
     axios.get('https://api.punkapi.com/v2/beers')
@@ -24,6 +25,27 @@ class App extends Component {
       this.setState({beerArray})
       console.log('beerArray', beerArray);
     })
+  }
+
+  handleClick(e, index) {
+    //looks through the beers to find the one the user clicks
+    const clickBeer = this.state.beerArray.filter((b, idx) => {
+      if(idx === index) {
+        return b
+      }})
+      
+      const liked = this.state.isLiked === false ? true : false
+
+    //adds the beer to the liked beers array
+    this.setState({
+      likedBeers: [...this.state.likedBeers, clickBeer],
+      isLiked: liked})
+    
+      
+    if(!liked) {
+      this.state.text = 'Liked'
+    } else {this.state.text = 'Like this beer?'}
+    console.log(this.state.text)
   }
 
   render() {
@@ -39,8 +61,11 @@ class App extends Component {
               tagline={beer.tagline}
               abv={beer.abv}
               description={beer.description}
-              image={beer.image_url}/>
-              
+              image={beer.image_url}
+              alt={beer.name}
+              onClick = {(e) => {this.handleClick(index)}}
+              value={this.state.text}
+              />
             })}</ol>
             
           </header>
--- src/BeerCard.js
+++ src/BeerCard.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 
 function BeerCard(props) {
-const { name, brewed, tagline, abv, description, image} = props;
+const { name, brewed, tagline, abv, description, image, alt, onClick, value } = props;
    return (
    <li style={{ listStyle: "none"}}>
-    <img style={{ height: "150px"}}src={image}></img>   
+    <img style={{ height: "150px"}}src={image} alt={alt}></img>   
        <h3>{name} <span> {brewed} </span></h3> 
        <h4>{tagline}</h4> 
     <p><span>{abv} ABV.</span> {description}</p>
-    <button>What does this say</button>
+    <button onClick={onClick} value={value}>{value}</button>
     </li>
    )
 }
